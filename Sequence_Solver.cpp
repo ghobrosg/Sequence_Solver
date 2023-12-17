5d67d8a73c7d56399fc40034e0d04358d674b05c
ghobrosg <94204190+ghobrosg@users.noreply.github.com>
2023-12-16 21:44:00 -0500

    Add files via upload

--- Sequence_Solver.cpp
+++ Sequence_Solver.cpp
@@ -5,6 +5,7 @@ using namespace std;
 double U1;
 double U2;
 double U3;
+char in;
 
 void arithmetic() {
     cout << "Arithmetic sequence" << endl;
@@ -24,18 +25,54 @@ void geometric() {
 void error() {
     cout << "Entered values do not make a sequence" << endl;
 }
-int main()
-{
+
+void optionx() {
+    cout << "Enter the first 3 values of the sequence" << endl;
     cin >> U1;
     cin >> U2;
     cin >> U3;
     if (U2 - U1 == U3 - U2) {
         arithmetic();
-} else if (U2/U1 == U3/U2){
-    geometric();
-    } else {
-    error();
     }
+    else if (U2 / U1 == U3 / U2) {
+        geometric();
+    }
+    else {
+        error();
+    }
+}
+
+void optiona() {
+    cout << "Enter the first 2 values of the arithmetic sequence" << endl;
+    cin >> U2;
+    cin >> U3;
+    arithmetic();
+}
+
+void optiong() {
+    cout << "Enter the first 2 values of the geometric sequence" << endl;
+    cin >> U2;
+    cin >> U3;
+    geometric();
+}
+
+int main()
+{
+    cout << "Enter the type of sequence\nInput 'a' if arithmetic, 'g' if geometric, or any other key if type is not known" << endl;
+    cin >> in;
+
+    if (in == 'a') {
+        optiona();
+    }
+
+    else if (in == 'g') {
+        optiong();
+    }
+
+    else {
+        optionx();
+    }
+
     system("pause");
     return 0;
 }
